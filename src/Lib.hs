29b770dbaf60e0e15b6c834088f833530eef9742
Elynn 3 <elynn@meowers.dev>
2023-12-22 20:46:20 -0300

    Add new code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,57 +1,80 @@
-module Lib
-    ( someFunc
-    ) where
-import Control.Monad.State (StateT, put, runStateT)
+module Lib where
 
-data Term
-  = Nil
-  | Number Integer  -- e.g. 5, 42
-  | Keyword String    -- e.g. x, first, lambda, -
-  | (:::) Term Term
+parse :: [Char] -> Program
+parse = undefined
+
+type Program = [SExp]
+
+data SExp
+  = Literal Integer -- e.g. 5, 42
+  | Symbol String -- e.g. x, first, lambda, -
+  | SExp [SExp] -- e.g. (+ (* 3 2) 4), (define fact (lambda n (fact (- n 1))))
+
+instance Show SExp where
+  show (Literal i) = show i
+  show (Symbol s) = show s
+  show (SExp list) = "(" ++ show list ++ ")"
 
 data Value
-  = VNumber Integer
-  | VList [Term]
-  | VLam (Value -> Eval Value)
+  = Num Integer
+  | List [SExp]
+  | Lam (Value -> Value)
 
-data Environment -- TODO
+instance Show Value where
+  show (List values) = "(" ++ show values ++ ")"
+  show (Num i) = show i
+  show (Lam _) = "<lambda>"
 
-type Program = [Term]
+type SymbolTable = [(String, Value)]
 
-type Eval a = StateT Environment (Either String) a
+-- Yes, a State monad is applicable here, but it's not really necessary
+-- for this exposition.
+eval :: SExp -> SymbolTable -> (Value, SymbolTable)
+-- If the expression is just an integer, it has no effect on the symbol table
+-- but we should print out that integer.
+eval (Literal n) table = (Num n, table)
+eval (Symbol s) table =
+  case lookup s table of
+    Just value -> (value, table)
+    Nothing -> error ("The value " ++ s ++ " is not defined! Aborting.")
+eval (SExp [Symbol "define", Symbol s, expr]) table =
+  let value = fst $ eval expr table
+   in -- Return the new value and update the symbol table, overwriting the old
+      -- definition in the stupidest way possible.
+      (value, (s, value) : table)
+-- Note that in real Lisps, the expression '(1 2 3) is a *reader shorthand*
+-- that expands to (quote 1 2 3).
+eval (SExp (Symbol "quote" : exprs)) table = (List exprs, table)
+eval (SExp [Symbol "lambda", Symbol s, expr]) table =
+  let fn value = fst $ eval expr ((s, value) : table)
+   in (Lam fn, table)
+-- This is the general case,
+eval (SExp (car : cdr)) table =
+  let (fn, _) = eval car table
+      args = map (fst . (`eval` table)) cdr
+   in (applyRecursive fn args, table)
+   where -- This will get us Curried function application.
+         applyRecursive :: Value -> [Value] -> Value
+         applyRecursive = foldr next where
+           next (Lam fn) value = fn value
+           next not_a_function _  = error ("Cannot apply non-function " ++ show not_a_function)
+eval _ _ = error "Fodeu"
 
-defaultEnvironment :: Environment
-defaultEnvironment = error "Implement a default environment"
+binop :: (Integer -> Integer -> Integer) -> Value
+binop f = Lam $ \lhs -> Lam $ \rhs -> Num $ f (unwrapInt lhs) (unwrapInt rhs)
 
-lookupInEnvironment :: String -> Environment -> Maybe Value
-lookupInEnvironment = error "Implement a lookup function based on a HashMap"
+standardLibrary :: SymbolTable
+standardLibrary = [("+", binop (+)),
+                   ("-", binop (-))]
 
-insertInEnvironment :: String -> Value -> Environment -> Environment
-insertInEnvironment = error "Implement an insert function based on a HashMap"
+unwrapInt :: Value -> Integer
+unwrapInt (Num i) = i
+unwrapInt _ = error "Expected integer"
 
-eval :: Term -> Environment -> Eval Value
-eval Nil _ = return $ VList []
-eval (Number n) _ = return $ VNumber n
-eval (Keyword name) environment = return $ case lookupInEnvironment name environment of
-  Just value -> value
-  Nothing -> error $ "Unknown keyword: " ++ name
-eval (Keyword "define" ::: Keyword name ::: expr ::: Nil) environment = do
-  value <- eval expr environment
-  put $ insertInEnvironment name value environment
-  return $ VList []
-eval (Keyword "lambda" ::: Keyword name ::: expr ::: Nil) environment = do
-  return $ VLam $ \value -> eval expr (insertInEnvironment name value environment)
-
-repl :: Program -> Environment -> Either String Value
-repl program environment = foldr evalAndPrint (Right ([], environment)) program
-  where
-    evalAndPrint :: Term -> Either String ([Value], Environment) -> Either String ([Value], Environment)
-    evalAndPrint term (Left left) = Left left
-    evalAndPrint term (Right (values, environment')) =
-      let value = runStateT (eval term environment') environment'
-      in case value of
-        Left err -> Left err
-        Right (value', environment'') -> Right (value' : values, environment'')
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+repl :: Program -> [Value]
+repl prog = fst $ foldr evalAndPrint ([], standardLibrary) prog
+ where
+  evalAndPrint :: SExp -> ([Value], SymbolTable) -> ([Value], SymbolTable)
+  evalAndPrint expr (values, table) =
+    let (newval, newtable) = eval expr table
+    in (newval : values, newtable)
